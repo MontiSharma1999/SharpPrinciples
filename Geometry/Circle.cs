d69e70d1ffca52c0b019d92c15cabd65b71766ca
MontiSharma1999 <113963712+MontiSharma1999@users.noreply.github.com>
2025-04-09 08:20:59 +0530

    SRP Situation

---
+++ Geometry/Circle.cs
@@ -0,0 +1,5 @@
+﻿namespace SharpPrinciples.Geometry;
+public class Circle(double radius)
+{
+    public double Radius { get; set; } = radius;
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
-﻿namespace SharpPrinciples;
+﻿using SharpPrinciples.Geometry;
+
+namespace SharpPrinciples;
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Circle circle = new Circle(5);
+        CalculateArea(circle);
+        Print(circle);
+    }
+    private static void CalculateArea(Circle circle)
+    {
+        double area = Math.PI * circle.Radius * circle.Radius;
+        Console.WriteLine(area);
+    }
+    private static void Print(Circle circle) 
+    {
+        Console.WriteLine(circle);
     }
 }
